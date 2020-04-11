11d4c603700995f4536dd0969dc5e3254e034ca6
thomas <2b72f8f1>
2020-04-11 10:56:02 +0700

    feature-test-1 done with squash

---
+++ app/Http/Controllers/ControllerTest1.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Post;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Config;
+
+class HomeController extends Controller
+{
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index(Request $request)
+    {
+        //git test
+
+        //Add something new here
+
+        // nearly fishnish my code
+
+
+        $post = Post::find($request->all());
+        return view('home.home', ['posts' => $posts]);
+    }
+}
